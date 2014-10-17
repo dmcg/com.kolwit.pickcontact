85b2a19a869bceb6aa3007d063807c9ce87705dd
Duncan McGregor <duncan@oneeyedmen.com>
2014-10-17 12:27:22 +0100

    Fix errors when no Activity is available

--- src/android/PickContactPlugin.java
+++ src/android/PickContactPlugin.java
@@ -17,35 +17,45 @@ public class PickContactPlugin extends CordovaPlugin {
 
     private Context context;
     private CallbackContext callbackContext;
-
+    private volatile boolean nastyRaceHack = false;
+        // When an ActivityNotFoundException is thrown by startActivityForResult
+        // sometimes callbackContext.error() is passed the exception message,
+        // and sometimes onActivityResult() is being invoked with RESULT_CANCELLED.
+        // At least that's what I think is happening.
+        
     private static final int CHOOSE_CONTACT = 1;
 
-	@Override
-	public boolean execute(String action, JSONArray data, CallbackContext callbackContext) {
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) {
         this.callbackContext = callbackContext;
-	    this.context = cordova.getActivity().getApplicationContext();
+        this.context = cordova.getActivity().getApplicationContext();
 
-		if (!action.equals("chooseContact"))
-		    return false;
+        if (!action.equals("chooseContact"))
+            return false;
 
         Intent intent = new Intent(Intent.ACTION_PICK, kindFrom(data));
         cordova.startActivityForResult(this, intent, CHOOSE_CONTACT);
+        nastyRaceHack = true;
 
         PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
         r.setKeepCallback(true);
         callbackContext.sendPluginResult(r);
         return true;
-	}
+    }
 
-	private Uri kindFrom(JSONArray data) {
-	    try {
-    	    return data.length() == 1 && data.getString(0).equals("email") ?
-                ContactsContract.CommonDataKinds.Email.CONTENT_URI :
-                ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+    private Uri kindFrom(JSONArray data) {
+        try {
+            if (data.length() == 0)
+                return ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+            if (data.getString(0).equals("email"))
+                return ContactsContract.CommonDataKinds.Email.CONTENT_URI;
+            if (data.getString(0).equals("none"))
+                return Uri.fromParts("content", "//no.such/thing", null);
+            return ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
         } catch (Exception x) {
             return ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
         }
-	}
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -58,10 +68,10 @@ public class PickContactPlugin extends CordovaPlugin {
             if (c.moveToFirst()) {
                 try {
                     String displayName = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
-					String selectedValue = c.getString(c.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME));
+                    String selectedValue = c.getString(c.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME));
                     JSONObject contact = new JSONObject();
-					contact.put("displayName", displayName);
-					contact.put("selectedValue", selectedValue);
+                    contact.put("displayName", displayName);
+                    contact.put("selectedValue", selectedValue);
                     callbackContext.success(contact);
 
                 } catch (Exception e) {
@@ -75,7 +85,10 @@ public class PickContactPlugin extends CordovaPlugin {
             c.close();
 
         } else if (resultCode == Activity.RESULT_CANCELED) {
-            callbackContext.error("No contact was selected.");
+            if (nastyRaceHack)
+                callbackContext.error("No contact was selected.");
+            else
+                callbackContext.error("No activity found");
         }
     }
 
